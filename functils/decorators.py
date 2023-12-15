d320b96deaa8d3a618fc7e92b8c0435e4fb3855f
Code Marnier <139389027+Codemariner-dev@users.noreply.github.com>
2023-12-15 14:51:44 +0100

    Update decorators.py

--- functils/decorators.py
+++ functils/decorators.py
@@ -1,4 +1,27 @@
 def memorized_method(func):
+    """
+    This decorator allow a user to cache a method call of an object (with no parameters).
+    The `force` parameter allow to update the cache value.
+    Internally, the first time the function is called (or when force°True), the returned value is stored in a new member.
+    Then, each new call will use the value stored in the cache.
+
+    ```python
+    class A:
+        @memorized_method
+        def func(self, force=False):
+            print("func called")
+            value = ... # expensive computation
+            return value
+
+    a = A()
+    a.func()
+    # func called
+    # value
+    a.func()
+    # value
+    ```
+    """
+
     def wrapper(obj, force=False):
         cache_attr = f"_memorized_method_cache_{func.__name__}"
         if not hasattr(obj, cache_attr) or force:
