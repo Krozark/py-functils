d01639f8218697fe53758fbb4d8a1146ce31242b
Code Marnier <139389027+Codemariner-dev@users.noreply.github.com>
2023-12-15 12:05:27 +0100

    Update decorators.py

--- functils/decorators.py
+++ functils/decorators.py
@@ -1,8 +1,8 @@
 def memorized_method(func):
-    def wrapper(obj, *args, force=False, **kwargs):
+    def wrapper(obj, force=False):
         cache_attr = f"_memorized_method_cache_{func.__name__}"
         if not hasattr(obj, cache_attr) or force:
-            result = func(obj, *args, **kwargs)
+            result = func(obj, force=force)
             setattr(obj, cache_attr, result)
         return getattr(obj, cache_attr)
 
